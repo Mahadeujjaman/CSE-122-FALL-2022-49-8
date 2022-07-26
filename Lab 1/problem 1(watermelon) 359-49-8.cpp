9023cdc1d7bf962ad4f6adfbfc2ea3063333402b
Mahadeuj Jaman <109549514+Mahadeujjaman@users.noreply.github.com>
2022-07-26 02:44:16 -0700

    Add files via upload

---
+++ Lab 1/problem 1(watermelon) 359-49-8.cpp
@@ -0,0 +1,18 @@
+//P-watermelon
+//id- 21225103359
+#include<iostream>
+using namespace std;
+int main()
+{
+int w;
+cin>>w;
+if(w%2==0 && w!=2)
+{
+cout<<"YES"<<endl;
+}
+else
+{
+cout<<"NO"<<endl;
+}
+return 0;
+}
